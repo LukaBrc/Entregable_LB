03584f4f4852a0673c7b0e9a95ce228884462ec8
LukaBrc <lucabeati@hotmail.com>
2022-08-29 20:28:00 -0300

    asdfasdf

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@
 // MÉTODOS: App debe tener un método para aumentar este número y que pueda ser ejecutado por su nieto Item.
 // PROPS: App deberá pasar por props lo necesario a sus componenetes internos.
 import React from "react";
-import Cabecera from "./components/Cabecera";
-import Listado from "./components/Listado"
+import Cabecera from "./components/Cabecera/Cabecera"
+import Listado from "./components/Listado/Listado"
 
 function App() {
 
